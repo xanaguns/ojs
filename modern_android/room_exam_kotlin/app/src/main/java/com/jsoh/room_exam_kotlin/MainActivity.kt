d23e53a0af8787e538bdabbf2479eb481e4f7404
gschoi <gunsoo.choi@bluebirdcorp.com>
2022-02-13 23:37:19 +0900

    implement the 1st version of room_exam_kotiln via Kotlin

--- modern_android/room_exam_kotlin/app/src/main/java/com/jsoh/room_exam_kotlin/MainActivity.kt
+++ modern_android/room_exam_kotlin/app/src/main/java/com/jsoh/room_exam_kotlin/MainActivity.kt
@@ -2,10 +2,25 @@ package com.jsoh.room_exam_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "todo-db"
+        ).allowMainThreadQueries()
+         .build()
+
+        result_text.text = db.todoDao().getAll().toString()
+
+        add_button.setOnClickListener {
+            db.todoDao().insert(Todo(todo_edit.text.toString()))
+            result_text.text = db.todoDao().getAll().toString()
+        }
     }
 }
---
+++ modern_android/room_exam_kotlin/app/src/main/java/com/jsoh/room_exam_kotlin/Todo.kt
@@ -0,0 +1,11 @@
+package com.jsoh.room_exam_kotlin
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Todo(
+    var title: String
+) {
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+}
