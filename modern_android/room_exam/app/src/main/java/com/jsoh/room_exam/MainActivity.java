c57729f22ddd86e07798dce3eea2e8f447db7068
gschoi <gunsoo.choi@bluebirdcorp.com>
2022-02-23 19:53:11 +0900

    applied ViewModel to room_exam

--- modern_android/room_exam/app/src/main/java/com/jsoh/room_exam/MainActivity.java
+++ modern_android/room_exam/app/src/main/java/com/jsoh/room_exam/MainActivity.java
@@ -1,12 +1,11 @@
 package com.jsoh.room_exam;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mTodoEditText;
@@ -20,32 +19,16 @@ public class MainActivity extends AppCompatActivity {
         mTodoEditText = findViewById(R.id.todo_edit);
         mResultTextView = findViewById(R.id.result_text);
 
-        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db")
-                .build();
+        MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
         // UI 갱신
-        db.todoDao().getAll().observe(this, todos -> {
+        viewModel.getAll().observe(this, todos -> {
             mResultTextView.setText(todos.toString());
         });
 
         // 버튼 클릭시 DB에 insert
         findViewById(R.id.add_button).setOnClickListener(v -> {
-            new InsertAsyncTask(db.todoDao())
-                    .execute(new Todo(mTodoEditText.getText().toString()));
+            viewModel.insert(new Todo(mTodoEditText.getText().toString()));
         });
-    }
-
-    private static class InsertAsyncTask extends AsyncTask<Todo, Void, Void> {
-        private TodoDao mTodoDao;
-
-        public InsertAsyncTask(TodoDao todoDao) {
-            this.mTodoDao = todoDao;
-        }
-
-        @Override
-        protected Void doInBackground(Todo... todos) {
-            mTodoDao.insert(todos[0]);
-            return null;
-        }
     }
 }
